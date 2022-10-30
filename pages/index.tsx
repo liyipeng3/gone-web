db4c1b7fc8e153519b53a442dfcf8700cfb386e4
李奕鹏 <45395042+liyipeng123@users.noreply.github.com>
2022-10-30 20:06:31 +0800

    fix: remove useless code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,15 @@
 import {logout, userSlice} from "@/store/user";
-import {useRef, useState} from "react";
-import {themeSlice} from "@/store/common/theme";
+import {useState} from "react";
 import Link from "next/link";
 import {useAppDispatch} from "@/store";
 import {Button} from "@/components/common/button";
 // import CompState from "@/components/test/state";
 
-export default function IndexScreen() {
+export default function Index() {
     const dispatch = useAppDispatch();
 
-    const testRef = useRef(Date.now());
     const setUser = () => {
         dispatch(userSlice.actions.login({uid, username}))
-        //dispatch(login({uid, username}))
-    }
-    const setMyTheme = (name: 'dark' | 'light') => {
-        dispatch(themeSlice.actions.setTheme({name}))
     }
     const [uid, setUid] = useState(-1)
     const [username, setUsername] = useState('未登录');
@@ -27,40 +21,30 @@ export default function IndexScreen() {
             <div className='main'>
                 <div className='box'>aa</div>
             </div>
-            {/*<CompState/>*/}
-            {/*<Counter />*/}
             uid: <input onChange={(e) => {
-            setUid(Number(e.target.value))
-        }}/>
+                setUid(Number(e.target.value))
+            }}/>
             username: <input onChange={(e) => {
-            setUsername(e.target.value)
-        }}/>
+                setUsername(e.target.value)
+            }}/>
             <Button onClick={() => {
                 setUser()
             }}>login
             </Button>
             {count}
-
             <button onClick={() => {
                 dispatch(logout())
             }}>logout
             </button>
             <button onClick={() => {
-                //setMyTheme('dark')
                 localStorage.theme = 'dark'
                 document.documentElement.classList.add('dark')
             }}>dark
             </button>
             <button onClick={() => {
-                //setMyTheme('light');
                 localStorage.theme = 'light'
                 document.documentElement.classList.remove('dark')
             }}>light
-            </button>
-            {/*<DemoComponent></DemoComponent>*/}
-            <button onClick={() => {
-                testRef.current = Date.now()
-            }}>ref
             </button>
         </div>
     )
