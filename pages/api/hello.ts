7b9be41edc04b8ffc158a775f3c7f90a3d5467e1
李奕鹏 <45395042+liyipeng123@users.noreply.github.com>
2023-02-12 14:09:25 +0800

    feat: test client info

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-interface Data {
-  name: string
-}
+// interface Data {
+//   name: string
+// }
 
+// export default function handler (
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+//
 export default function handler (
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ): void {
-  res.status(200).json({ name: 'lyp123' })
+  // console.log(req.headers['user-agent'])
+  const ua: string = req.headers['user-agent'] ?? ''
+  const uaReg: RegExp = /Mozilla\/5.0\s*\([^()]*?(Windows[^()]*?|Android[^()]*?|Mac OS[^()]*?|iPhone)(\)|;\s*([^()]*?)\))/
+  const uaMatch: RegExpMatchArray | null = ua.match(uaReg)
+  let device = 'unknown'
+  if (uaMatch != null) {
+    device = uaMatch[1].replaceAll(/NT./g, '').replaceAll('_', '.')
+  }
+  let ip = (req.headers['x-forwarded-for'] ?? req.headers['x-real-ip'] ?? req.socket.remoteAddress) as string
+  if (ip.startsWith('::ffff:')) {
+    ip = ip.slice(7)
+  }
+  const info = {
+    ip,
+    // ua: req.headers['user-agent'] ?? '',
+    device
+  }
+  res.status(200).json(info)
 }
