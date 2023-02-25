bb833f7bc483c0c32def8a0913613d210b914cbd
李奕鹏 <45395042+liyipeng123@users.noreply.github.com>
2023-02-25 12:42:10 +0800

    fix: _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,23 +2,32 @@ import '@/styles/globals.scss'
 import 'highlight.js/scss/github-dark-dimmed.scss'
 
 import NextApp, { type AppContext, type AppProps } from 'next/app'
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Layout } from '@/components/layout'
 import prisma from '@/utils/prisma'
-import { type HotList } from '@/types'
 
 interface CustomAppProps {
   visitTimes: number
-  hotList: HotList
 }
 
-const App: React.FC<AppProps & CustomAppProps> = ({
-  Component,
-  pageProps,
-  visitTimes,
-  hotList
-}) => {
-  useEffect(() => {
+class App extends NextApp {
+  static async getInitialProps (content: AppContext) {
+    const initialProps = await NextApp.getInitialProps(content)
+    const visitTimesData = await prisma.options?.findFirst({
+      where: {
+        name: 'visitTimes'
+      },
+      select: {
+        value: true
+      }
+    })
+    return {
+      ...initialProps,
+      visitTimes: visitTimesData?.value ?? 0
+    }
+  }
+
+  componentDidMount () {
     const v = sessionStorage.getItem('_v')
     if (v == null) {
       sessionStorage.setItem('_v', '1')
@@ -27,31 +36,19 @@ const App: React.FC<AppProps & CustomAppProps> = ({
       }).then(_ => undefined)
     }
     console.log('%c lyp123 %c https://lyp123.com ', 'color: #fff; margin: 1em 0; padding: 5px 0; background: #00a9e0;', 'margin: 1em 0; padding: 5px 0; background: #efefef;')
-  }, [])
+  }
 
-  return (
-    <Layout visitTimes={visitTimes}>
-      <Component {...pageProps}/>
-    </Layout>
-  )
-}
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-App.getInitialProps = async (context: AppContext) => {
-  const ctx = await NextApp.getInitialProps(context)
-  const visitTimesData = await prisma.options?.findFirst({
-    where: {
-      name: 'visitTimes'
-    },
-    select: {
-      value: true
-    }
-  })
-
-  return {
-    ...ctx,
-    visitTimes: visitTimesData?.value ?? 0
+  render () {
+    const {
+      Component,
+      pageProps,
+      visitTimes
+    } = this.props as AppProps & CustomAppProps
+    return (
+      <Layout visitTimes={visitTimes}>
+        <Component {...pageProps}/>
+      </Layout>
+    )
   }
 }
 
