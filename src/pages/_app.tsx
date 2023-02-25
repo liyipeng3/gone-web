1a4d70045116007843e423ce3517c4158e2c60cc
李奕鹏 <45395042+liyipeng123@users.noreply.github.com>
2023-02-25 13:32:59 +0800

    fix: restore _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.scss'
 import 'highlight.js/scss/github-dark-dimmed.scss'
 
 import NextApp, { type AppContext, type AppProps } from 'next/app'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Layout } from '@/components/layout'
 import prisma from '@/utils/prisma'
 
@@ -10,24 +10,12 @@ interface CustomAppProps {
   visitTimes: number
 }
 
-class App extends NextApp {
-  static async getInitialProps (content: AppContext) {
-    const initialProps = await NextApp.getInitialProps(content)
-    const visitTimesData = await prisma.options?.findFirst({
-      where: {
-        name: 'visitTimes'
-      },
-      select: {
-        value: true
-      }
-    })
-    return {
-      ...initialProps,
-      visitTimes: visitTimesData?.value ?? 0
-    }
-  }
-
-  componentDidMount () {
+const App: React.FC<AppProps & CustomAppProps> & { getInitialProps: any } = ({
+  Component,
+  pageProps,
+  visitTimes
+}) => {
+  useEffect(() => {
     const v = sessionStorage.getItem('_v')
     if (v == null) {
       sessionStorage.setItem('_v', '1')
@@ -36,19 +24,29 @@ class App extends NextApp {
       }).then(_ => undefined)
     }
     console.log('%c lyp123 %c https://lyp123.com ', 'color: #fff; margin: 1em 0; padding: 5px 0; background: #00a9e0;', 'margin: 1em 0; padding: 5px 0; background: #efefef;')
-  }
+  }, [])
 
-  render () {
-    const {
-      Component,
-      pageProps,
-      visitTimes
-    } = this.props as AppProps & CustomAppProps
-    return (
-      <Layout visitTimes={visitTimes}>
-        <Component {...pageProps}/>
-      </Layout>
-    )
+  return (
+    <Layout visitTimes={visitTimes}>
+      <Component {...pageProps}/>
+    </Layout>
+  )
+}
+
+App.getInitialProps = async (context: AppContext) => {
+  const ctx = await NextApp.getInitialProps(context)
+  const visitTimesData = await prisma.options?.findFirst({
+    where: {
+      name: 'visitTimes'
+    },
+    select: {
+      value: true
+    }
+  })
+
+  return {
+    ...ctx,
+    visitTimes: visitTimesData?.value ?? 0
   }
 }
 
