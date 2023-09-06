496113bacaacf6587fe404689a05b18c7471ed91
李奕鹏 <17895987@qq.com>
2023-09-07 00:30:24 +0800

    feat: update deps and fix db

--- src/utils/marked.ts
+++ src/utils/marked.ts
@@ -1,11 +1,12 @@
-import { marked } from 'marked'
+import { Marked } from 'marked'
 import highlight from 'highlight.js'
+import { markedHighlight } from 'marked-highlight'
 
-marked.setOptions({
+const marked = new Marked(markedHighlight({
   highlight: function (code, lang) {
     const language = (highlight.getLanguage(lang) != null) ? lang : 'plaintext'
     return highlight.highlightAuto(code, [language]).value
   }
-})
+}))
 
 export default marked
