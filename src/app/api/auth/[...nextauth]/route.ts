37e92f21cf481c86d9751d81c05d4b979c6458b2
李奕鹏 <17895987@qq.com>
2023-09-10 15:55:23 +0800

    fix: types

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 
-export const authOptions = {
+const authOptions = {
   // Configure one or more authentication providers
   providers: [
     CredentialsProvider({
