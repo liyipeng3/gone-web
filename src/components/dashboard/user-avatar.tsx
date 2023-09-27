23d67b6f0e30021a710b2153f46fbd7b7c7b297a
李奕鹏 <17895987@qq.com>
2023-09-28 00:49:15 +0800

    fix: types

--- src/components/dashboard/user-avatar.tsx
+++ src/components/dashboard/user-avatar.tsx
@@ -1,11 +1,10 @@
-import { type User } from '@prisma/client'
 import { type AvatarProps } from '@radix-ui/react-avatar'
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Icons } from '@/components/common/icons'
 
 interface UserAvatarProps extends AvatarProps {
-  user: Pick<User, 'image' | 'name'>
+  user: any
 }
 
 export function UserAvatar ({ user, ...props }: UserAvatarProps) {
