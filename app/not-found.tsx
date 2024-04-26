873a22e55daba828010108a13b4080ff3d98a472
李奕鹏 <17895987@qq.com>
2024-04-27 03:10:10 +0800

    feat: 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+
+export default function NotFound () {
+  return (
+    <div className="flex flex-col bg-gray-50 items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-4">404 - Not Found</h1>
+      <p className="text-gray-600">Sorry, the page you&apos;re looking for doesn&apos;t exist.</p>
+
+      {/* <div className="mt-6"> */}
+      {/*   <input */}
+      {/*     type="text" */}
+      {/*     placeholder="Search for content..." */}
+      {/*     className="px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500" */}
+      {/*   /> */}
+      {/* </div> */}
+
+      <div className="mt-4 text-center">
+        {/* <p className="text-gray-600 mb-3">Or, you might want to explore:</p> */}
+        <Link href="/" className="text-blue-500 hover:underline">Home</Link>
+        {/* <Link href="/blog" className="text-blue-500 hover:underline ml-2">Blog</Link> */}
+        {/* <Link href="/contact" className="text-blue-500 hover:underline ml-2">Contact</Link> */}
+      </div>
+    </div>
+  )
+}
