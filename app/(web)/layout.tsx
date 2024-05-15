d99a0c2dcef0a7f6eadd6f1d2a6f1b2e854593cc
李奕鹏 <17895987@qq.com>
2024-05-16 01:29:52 +0800

    feat: rm

--- app/(web)/layout.tsx
+++ app/(web)/layout.tsx
@@ -5,8 +5,6 @@ import { Layout } from '@/components/layout'
 import AllView from '@/components/custom/View/all'
 
 export default function RootLayout ({
-  // Layouts must accept a children prop.
-  // This will be populated with nested layouts or pages
   children
 }: {
   children: React.ReactNode
