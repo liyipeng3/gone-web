1358e289659b29ba6e43c02ffe024636d9d2d162
李奕鹏 <17895987@qq.com>
2024-05-19 15:11:55 +0800

    fix: config

--- next.config.js
+++ next.config.js
@@ -8,7 +8,6 @@ const nextConfig = {
   images: {
     domains: [],
   },
-  output: 'standalone',
 }
 
 
--- pm2.config.js
+++ pm2.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   apps: [{
     name: 'gone-web',
-    script: 'node',
-    args: '.next/standalone/server.js',
+    script: 'node_modules/next/dist/bin/next',
+    args: 'start',
     watch: false,
     ignore_watch: ['node_modules', 'logs'],
     // out_file: 'logs/out.log',
