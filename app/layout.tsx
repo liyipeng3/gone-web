301736882f66dfa3866d0283938e16a2e7d90de7
李奕鹏 <17895987@qq.com>
2024-05-23 01:32:37 +0800

    feat: add full screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,7 @@ export default function RootLayout ({
     <html lang="en">
     <head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0"/>
+      <meta name="apple-mobile-web-app-capable" content="yes"/>
       <meta name="theme-color" content="#ffffff"/>
       <script
         dangerouslySetInnerHTML={{
