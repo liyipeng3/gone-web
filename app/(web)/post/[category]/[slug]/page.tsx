2f28e34bdd89c19ef86fc47f5dfe5e3a52a01037
李奕鹏 <17895987@qq.com>
2025-03-19 00:33:53 +0800

    feat: link

--- app/(web)/post/[category]/[slug]/page.tsx
+++ app/(web)/post/[category]/[slug]/page.tsx
@@ -7,7 +7,7 @@ import { getPagePostInfo } from '@/services/post'
 import PostView from '@/components/custom/View/post'
 import { type Metadata } from 'next/types'
 import CommentList from '@/components/common/comment'
-
+import Link from 'next/link'
 export async function generateMetadata (
   { params }: { params: { slug: string } }
 ): Promise<Metadata> {
@@ -45,7 +45,7 @@ const Content: React.FC<{ params: { slug: string } }> = async (
           <div className="text-xs mb-5 -mt-3 md:mt-3 my-3 text-gray-500 space-x-1.5 dark:text-gray-400">
             <span>{dayjs(new Date(created * 1000)).format('YYYY-MM-DD HH:MM')}</span>
             <span>•</span>
-            <span>{name}</span>
+            <Link href={`/category/${category as string}`} className="text-gray-500 dark:text-gray-400 no-underline font-normal">{name}</Link>
             <span>•</span>
             <span>{viewsNum}人阅读</span>
           </div>
