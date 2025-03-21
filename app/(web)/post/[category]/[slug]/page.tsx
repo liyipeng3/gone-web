8a34d66d0d3e5c6e46f96c3761a86514dbfa6a18
李奕鹏 <17895987@qq.com>
2025-03-22 00:55:22 +0800

    feat: add min height

--- app/(web)/post/[category]/[slug]/page.tsx
+++ app/(web)/post/[category]/[slug]/page.tsx
@@ -8,6 +8,7 @@ import PostView from '@/components/custom/View/post'
 import { type Metadata } from 'next/types'
 import CommentList from '@/components/common/comment'
 import Link from 'next/link'
+
 export async function generateMetadata (
   { params }: { params: { slug: string } }
 ): Promise<Metadata> {
@@ -36,16 +37,17 @@ const Content: React.FC<{ params: { slug: string } }> = async (
   return (
     <Main hotList={hotList}>
       <div className="md:max-w-3xl max-w-full md:min-w-[48rem]">
-        <article className="md:max-w-3xl max-w-full md:min-w-[48rem] text-left flex-1 prose">
+        <article className="md:max-w-3xl max-w-full md:min-w-[48rem] text-left flex-1 prose min-h-[30vh]">
           <Breadcrumb items={[{
             name,
             href: `/category/${category as string}`
           }, { name: '正文' }]}/>
           <h2 className="md:mb-2 mt-4 dark:text-white">{title}</h2>
-          <div className="text-xs mb-5 -mt-3 md:mt-3 my-3 text-gray-500 space-x-1.5 dark:text-gray-400">
+          <div className="text-xs mb-5 -mt-3 md:mt-3 my-3 text-gray-500 space-x-1.5 dark:text-gray-400 ">
             <span>{dayjs(new Date(created * 1000)).format('YYYY-MM-DD HH:MM')}</span>
             <span>•</span>
-            <Link href={`/category/${category as string}`} className="text-gray-500 dark:text-gray-400 no-underline font-normal">{name}</Link>
+            <Link href={`/category/${category as string}`}
+                  className="text-gray-500 dark:text-gray-400 no-underline font-normal">{name}</Link>
             <span>•</span>
             <span>{viewsNum}人阅读</span>
           </div>
