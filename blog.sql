69c9dd0e3b6582727dfb4adc509ec1911a407c9a
李奕鹏 <17895987@qq.com>
2025-04-26 11:31:39 +0800

    feat: update sql

--- blog.sql
+++ blog.sql
@@ -1,39 +1,18 @@
--- phpMyAdmin SQL Dump
--- version 4.4.15.10
--- https://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: 2024-09-30 18:21:45
--- 服务器版本： 5.7.34-log
--- PHP Version: 5.6.40
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `blog-dev`
---
-
--- --------------------------------------------------------
-
---
--- 表的结构 `comments`
---
-
-CREATE TABLE IF NOT EXISTS `comments` (
-  `coid` int(10) unsigned NOT NULL,
+-- ----------------------------
+-- Table structure for comments
+-- ----------------------------
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments` (
+  `coid` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `cid` int(10) unsigned DEFAULT '0',
   `created` int(10) unsigned DEFAULT '0',
   `author` varchar(200) DEFAULT NULL,
   `authorId` int(10) unsigned DEFAULT '0',
   `ownerId` int(10) unsigned DEFAULT '0',
-  `mail` varchar(200) DEFAULT NULL,
+  `email` varchar(200) DEFAULT NULL,
   `url` varchar(200) DEFAULT NULL,
   `ip` varchar(64) DEFAULT NULL,
   `agent` varchar(200) DEFAULT NULL,
@@ -41,93 +20,89 @@ CREATE TABLE IF NOT EXISTS `comments` (
   `type` varchar(16) DEFAULT 'comment',
   `status` varchar(16) DEFAULT 'approved',
   `parent` int(10) unsigned DEFAULT '0',
-  `authorImg` varchar(500) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `reply` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`coid`),
+  KEY `cid` (`cid`),
+  KEY `created` (`created`),
+  CONSTRAINT `comments_posts_cid_fk` FOREIGN KEY (`cid`) REFERENCES `posts` (`cid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2174 DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- 表的结构 `fields`
---
-
-CREATE TABLE IF NOT EXISTS `fields` (
+-- ----------------------------
+-- Table structure for fields
+-- ----------------------------
+DROP TABLE IF EXISTS `fields`;
+CREATE TABLE `fields` (
   `cid` int(10) unsigned NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `type` varchar(8) DEFAULT 'str',
-  `str_value` text,
-  `int_value` int(11) DEFAULT '0',
-  `float_value` float DEFAULT '0'
+  `type` varchar(200) NOT NULL,
+  `value` text,
+  PRIMARY KEY (`cid`,`type`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- 表的结构 `gallery`
---
-
-CREATE TABLE IF NOT EXISTS `gallery` (
-  `gid` int(10) unsigned NOT NULL COMMENT 'gallery表主键',
+-- ----------------------------
+-- Table structure for gallery
+-- ----------------------------
+DROP TABLE IF EXISTS `gallery`;
+CREATE TABLE `gallery` (
+  `gid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'gallery表主键',
   `thumb` varchar(200) DEFAULT NULL COMMENT '缩略图',
   `image` varchar(200) DEFAULT NULL COMMENT '原图',
   `sort` int(11) DEFAULT '0' COMMENT '相册组',
   `name` varchar(200) DEFAULT NULL COMMENT '图片名称',
   `description` varchar(200) DEFAULT NULL COMMENT '图片描述',
-  `order` int(10) unsigned DEFAULT '0' COMMENT '图片排序'
+  `order` int(10) unsigned DEFAULT '0' COMMENT '图片排序',
+  PRIMARY KEY (`gid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- 表的结构 `links`
---
-
-CREATE TABLE IF NOT EXISTS `links` (
-  `lid` int(10) unsigned NOT NULL COMMENT 'links表主键',
+-- ----------------------------
+-- Table structure for links
+-- ----------------------------
+DROP TABLE IF EXISTS `links`;
+CREATE TABLE `links` (
+  `lid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'links表主键',
   `name` varchar(200) DEFAULT NULL COMMENT 'links名称',
   `url` varchar(200) DEFAULT NULL COMMENT 'links网址',
   `sort` varchar(200) DEFAULT NULL COMMENT 'links分类',
   `image` varchar(200) DEFAULT NULL COMMENT 'links图片',
   `description` varchar(200) DEFAULT NULL COMMENT 'links描述',
   `user` varchar(200) DEFAULT NULL COMMENT '自定义',
-  `order` int(10) unsigned DEFAULT '0' COMMENT 'links排序'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `order` int(10) unsigned DEFAULT '0' COMMENT 'links排序',
+  PRIMARY KEY (`lid`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- 表的结构 `metas`
---
-
-CREATE TABLE IF NOT EXISTS `metas` (
-  `mid` int(10) unsigned NOT NULL,
+-- ----------------------------
+-- Table structure for metas
+-- ----------------------------
+DROP TABLE IF EXISTS `metas`;
+CREATE TABLE `metas` (
+  `mid` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(200) DEFAULT NULL,
   `slug` varchar(200) DEFAULT NULL,
   `type` varchar(32) NOT NULL,
   `description` varchar(200) DEFAULT NULL,
   `count` int(10) unsigned DEFAULT '0',
   `order` int(10) unsigned DEFAULT '0',
-  `parent` int(10) unsigned DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `parent` int(10) unsigned DEFAULT '0',
+  PRIMARY KEY (`mid`),
+  UNIQUE KEY `metas_pk` (`slug`,`type`),
+  KEY `slug` (`slug`)
+) ENGINE=InnoDB AUTO_INCREMENT=71 DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- 表的结构 `options`
---
-
-CREATE TABLE IF NOT EXISTS `options` (
+-- ----------------------------
+-- Table structure for options
+-- ----------------------------
+DROP TABLE IF EXISTS `options`;
+CREATE TABLE `options` (
   `name` varchar(32) NOT NULL,
-  `value` int(11) NOT NULL
+  `value` int(11) NOT NULL,
+  PRIMARY KEY (`name`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- 表的结构 `posts`
---
-
-CREATE TABLE IF NOT EXISTS `posts` (
-  `cid` int(10) unsigned NOT NULL,
+-- ----------------------------
+-- Table structure for posts
+-- ----------------------------
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE `posts` (
+  `cid` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `uid` int(10) unsigned DEFAULT '1',
   `title` varchar(200) DEFAULT NULL,
   `slug` varchar(200) DEFAULT NULL,
@@ -142,168 +117,48 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `allowComment` char(1) DEFAULT '0',
   `parent` int(10) unsigned DEFAULT '0',
   `viewsNum` int(11) DEFAULT '0',
-  `likesNum` int(11) DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `likesNum` int(11) DEFAULT '0',
+  PRIMARY KEY (`cid`),
+  UNIQUE KEY `slug` (`slug`),
+  UNIQUE KEY `unique-slug` (`slug`) USING BTREE,
+  KEY `created` (`created`),
+  KEY `posts_users_uid_fk` (`uid`),
+  CONSTRAINT `posts_users_uid_fk` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=293 DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- 表的结构 `relationships`
---
-
-CREATE TABLE IF NOT EXISTS `relationships` (
+-- ----------------------------
+-- Table structure for relationships
+-- ----------------------------
+DROP TABLE IF EXISTS `relationships`;
+CREATE TABLE `relationships` (
   `cid` int(10) unsigned NOT NULL,
-  `mid` int(10) unsigned NOT NULL
+  `mid` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`cid`,`mid`),
+  KEY `cid` (`cid`),
+  KEY `mid` (`mid`),
+  CONSTRAINT `cid` FOREIGN KEY (`cid`) REFERENCES `posts` (`cid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `mid` FOREIGN KEY (`mid`) REFERENCES `metas` (`mid`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- 表的结构 `users`
---
-
-CREATE TABLE IF NOT EXISTS `users` (
-  `uid` int(10) unsigned NOT NULL,
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `uid` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(32) DEFAULT NULL,
   `password` varchar(64) DEFAULT NULL,
-  `mail` varchar(200) DEFAULT NULL,
+  `email` varchar(200) DEFAULT NULL,
   `url` varchar(200) DEFAULT NULL,
-  `screenName` varchar(32) DEFAULT NULL,
+  `nickname` varchar(32) DEFAULT NULL,
   `created` int(10) unsigned DEFAULT '0',
   `activated` int(10) unsigned DEFAULT '0',
   `logged` int(10) unsigned DEFAULT '0',
   `group` varchar(16) DEFAULT 'visitor',
-  `authCode` varchar(64) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `authCode` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`uid`),
+  UNIQUE KEY `name` (`username`),
+  UNIQUE KEY `mail` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `comments`
---
-ALTER TABLE `comments`
-  ADD PRIMARY KEY (`coid`),
-  ADD KEY `cid` (`cid`),
-  ADD KEY `created` (`created`);
-
---
--- Indexes for table `fields`
---
-ALTER TABLE `fields`
-  ADD PRIMARY KEY (`cid`,`name`),
-  ADD KEY `int_value` (`int_value`),
-  ADD KEY `float_value` (`float_value`);
-
---
--- Indexes for table `gallery`
---
-ALTER TABLE `gallery`
-  ADD PRIMARY KEY (`gid`);
-
---
--- Indexes for table `links`
---
-ALTER TABLE `links`
-  ADD PRIMARY KEY (`lid`);
-
---
--- Indexes for table `metas`
---
-ALTER TABLE `metas`
-  ADD PRIMARY KEY (`mid`),
-  ADD KEY `slug` (`slug`);
-
---
--- Indexes for table `options`
---
-ALTER TABLE `options`
-  ADD PRIMARY KEY (`name`) USING BTREE;
-
---
--- Indexes for table `posts`
---
-ALTER TABLE `posts`
-  ADD PRIMARY KEY (`cid`),
-  ADD UNIQUE KEY `slug` (`slug`),
-  ADD UNIQUE KEY `unique-slug` (`slug`) USING BTREE,
-  ADD KEY `created` (`created`),
-  ADD KEY `posts_users_uid_fk` (`uid`);
-
---
--- Indexes for table `relationships`
---
-ALTER TABLE `relationships`
-  ADD PRIMARY KEY (`cid`,`mid`),
-  ADD KEY `cid` (`cid`),
-  ADD KEY `mid` (`mid`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`uid`),
-  ADD UNIQUE KEY `name` (`username`),
-  ADD UNIQUE KEY `mail` (`mail`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `comments`
---
-ALTER TABLE `comments`
-  MODIFY `coid` int(10) unsigned NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `gallery`
---
-ALTER TABLE `gallery`
-  MODIFY `gid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'gallery表主键';
---
--- AUTO_INCREMENT for table `links`
---
-ALTER TABLE `links`
-  MODIFY `lid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'links表主键';
---
--- AUTO_INCREMENT for table `metas`
---
-ALTER TABLE `metas`
-  MODIFY `mid` int(10) unsigned NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `posts`
---
-ALTER TABLE `posts`
-  MODIFY `cid` int(10) unsigned NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `uid` int(10) unsigned NOT NULL AUTO_INCREMENT;
---
--- 限制导出的表
---
-
---
--- 限制表 `comments`
---
-ALTER TABLE `comments`
-  ADD CONSTRAINT `comments_posts_cid_fk` FOREIGN KEY (`cid`) REFERENCES `posts` (`cid`);
-
---
--- 限制表 `posts`
---
-ALTER TABLE `posts`
-  ADD CONSTRAINT `posts_users_uid_fk` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`);
-
---
--- 限制表 `relationships`
---
-ALTER TABLE `relationships`
-  ADD CONSTRAINT `cid` FOREIGN KEY (`cid`) REFERENCES `posts` (`cid`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `mid` FOREIGN KEY (`mid`) REFERENCES `metas` (`mid`) ON DELETE CASCADE ON UPDATE CASCADE;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET FOREIGN_KEY_CHECKS = 1;
