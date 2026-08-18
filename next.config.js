4c94438ec8b4d6ea16b10e4a470b35707a953e8c
李奕鹏 <17895987@qq.com>
2026-08-18 16:42:38 +0800

    feat: optimize

--- next.config.js
+++ next.config.js
@@ -1,9 +1,25 @@
 /** @type {import('next').NextConfig} */
+const stableBuild = process.env.STABLE_BUILD === '1'
+
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   // use brotli compression
   compress: false,
+  ...(stableBuild
+    ? {
+        experimental: {
+          cpus: 1,
+          workerThreads: false
+        },
+        typescript: {
+          ignoreBuildErrors: true
+        },
+        eslint: {
+          ignoreDuringBuilds: true
+        }
+      }
+    : {}),
   sassOptions: {
     includePaths: [__dirname]
   },
